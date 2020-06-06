74585f05d6385aae883f6f0b581ce1953ba2bd5a
Yurii.Moroziuk <Yuriy.Morozyuk.95@gmail.com>
2020-06-06 18:25:21 +0300

    add Unit Of Work

---
+++ MyWorkout.Web/Data/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace MyWorkout.Web.Data.Repositories
+{
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> EntitySet { get; }
+        IAsyncEnumerable<T> ReadAll();
+        Task<T> Read(int id);
+        Task Create(T item);
+        void Update(T item);
+        Task Delete(int id);
+    }
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbSet<T> dbSet)
+        {
+            _dbSet = dbSet;
+        }
+        //TODO remove after DAL will be done
+        public DbSet<T> EntitySet => _dbSet;
+
+        public IAsyncEnumerable<T> ReadAll()
+        {
+            return _dbSet.AsAsyncEnumerable();
+        }
+        public async Task<T> Read(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public void Update(T entity)
+        {
+             _dbSet.Update(entity);
+        }
+        public async Task Delete(int id)
+        {
+            T game = await _dbSet.FindAsync(id);
+
+            if (game != null)
+            {
+               _dbSet.Remove(game);
+            }
+        }
+    }
+}
